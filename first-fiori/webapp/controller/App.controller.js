37dd8aca0dd5a558ee360ef7936b71480a7a7c9b
rojas <rojas.devnet@gmail.com>
2025-11-11 17:40:20 -0700

    models

---
+++ first-fiori/webapp/controller/App.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define(
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+  ],
+  function (Controller, MessageToast, JSONModel) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.App", {
+      onInit: function () {
+        // set the data model on the view
+        var oData = {
+          recipient: {
+            name: "UI5",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.getView().setModel(oModel);
+      },
+
+      onShowHello: function () {
+        //show native or vanilla js
+        MessageToast.show("Hello from onShowHello event");
+      },
+      onClick: function () {
+        MessageToast.show("Hello from onClick event");
+      },
+    });
+  }
+);
